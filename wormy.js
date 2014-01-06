0061e862d227a92b04b132fcec4e27528bd0835e
Patrick Plunkett <plunkett@sam.(none)>
2014-01-06 14:24:25 -0800

    Changed the animation method for the starting sequence

--- wormy.js
+++ wormy.js
@@ -1,72 +1,55 @@
 var canvas;
 var context;
 
-var m_x = 100;
-var m_y = 100;
-
-var m_down = 0;
-var grabbed = 0;
-var paused = 0;
-
-var M=new Number(1);
-var L=new Number(100);
-var DT=new Number(0.05);
-var THETA1=new Number(Math.PI/2);
-var THETA2=new Number(Math.PI/2);
-var PTHETA1=new Number(0);
-var PTHETA2=new Number(0);
-
-var m_input;
-var l_input;
-var int_id = NaN;
 var startscreen_id = NaN;
 
 function init() {
-
+    
     canvas = document.getElementById("canvas");
     
     if( canvas.getContext ) {
-    
-		context = canvas.getContext("2d");
-		context.font = "20px";
-		
-		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight;
-		
-		// int_id = setInterval(draw,DT*100);
-		
-		// pygame_draw_rect("#f50", pygame_Rect(30, 10, 100, 300));
-		
-		showStartScreen();
+        
+	context = canvas.getContext("2d");
+	context.font = "20px";
+	
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	
+	// int_id = setInterval(draw,DT*100);
+	
+	// pygame_draw_rect("#f50", pygame_Rect(30, 10, 100, 300));
+
+	showStartScreen(0);
     }
 }
 
 function make_rect(x, y, w, h) {
-	return {x:x, y:y, w:w, h:h};
+    return {x:x, y:y, w:w, h:h};
 }
 
 function draw_rect(color, rect) {
 
-	context.save();
-	
-	context.fillStyle = color;
-	context.rect(rect.x, rect.y, rect.w, rect.h);
-	context.fill();
-	
-	context.restore();
+    context.save();
+    
+    context.fillStyle = color;
+    context.rect(rect.x, rect.y, rect.w, rect.h);
+    context.fill();
+    
+    context.restore();
 }
 
 function erase() {
-    draw_rect("#000", make_rect(0,0,canvas.width,canvas.height));
+    context.clearRect(0,0,canvas.width,canvas.height);
+    //draw_rect("#000", make_rect(0,0,canvas.width,canvas.height));
 }
 
 function drawTitleText(angle1, angle2) {
 	
-	context.save();
-	
-	erase();
-	context.font = "100px Arial";
-	
+    context.save();
+    
+    erase();
+    context.font = "100px Arial";
+    
     context.save();
     context.fillStyle = "#0f0";
     context.textAlign = "center";
@@ -88,18 +71,12 @@ function drawTitleText(angle1, angle2) {
     context.restore();
 }
 
-function showStartScreen() {
-	
-	var angle1 = 0.0;
-    var angle2 = 0.0;
-    
-	startscreen_id = setInterval(function() {
-		angle1 += 0.05;
-		angle2 += 0.09;
-		
-		drawTitleText(angle1, angle2);
-	}, 50);
-        
+function showStartScreen(t) {
+    startscreen_id = requestAnimationFrame(showStartScreen);
+
+    var angle1 = 0.25 * (2*Math.PI*t/1000);
+    var angle2 = 0.15 * (2*Math.PI*t/1000);
+    drawTitleText(angle1, angle2);    
 }
 
 
@@ -127,20 +104,43 @@ function key_press(event) {
     var code = event.keyCode?event.keyCode:event.which;
     
     if( !isNaN(startscreen_id) ) {
-		clearInterval(startscreen_id);
-		startscreen_id = NaN;
-	}
+	cancelAnimationFrame(startscreen_id);
+	startscreen_id = NaN;
+    }
     
     switch(code) {
-		case 112:
-			if( paused == 0 ) {
-				paused = 1;
-			} else {
-				paused = 0;
-			}
-		break;
+    case 112:
+	if( paused == 0 ) {
+	    paused = 1;
+	} else {
+	    paused = 0;
+	}
+	break;
     }
     
 }
 
-
+(function() {
+    var lastTime = 0;
+    var vendors = ['webkit', 'moz'];
+    for(var v = 0; v < vendors.length && !window.requestAnimationFrame; v++) {
+        window.requestAnimationFrame = window[vendors[v]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame =
+            window[vendors[v]+'CancelAnimationFrame'] || window[vendors[v]+'CancelRequestAnimationFrame'];
+    }
+    
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+                                       timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+    
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
